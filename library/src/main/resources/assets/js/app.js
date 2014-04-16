0c82c9b26612ef0450b03c71261c19998042ea48
Ubuntu <ubuntu@ip-172-31-20-214.us-west-1.compute.internal>
2014-04-16 06:43:43 +0000

    upload files

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,18 @@
+var URL = "http://54.193.40.29:8001/library";
 $(":button").click(function() {
-	var isbn = this.id;
-	alert('About to report lost on ISBN ' + isbn);
+    var isbn = this.id;
+    alert('About to report lost on ISBN ' + isbn);
+    var tr = $(this).closest('tr');
+    var id = tr.find('#status');
+    alert(id.text());
+	$.ajax({
+	    type: "PUT",
+	    url: URL + '/v1/books/'+ isbn + '?status=lost',
+	    contentType: "application/json",
+	    success: function(data){
+		    	alert(JSON.stringify(data)+' '+data.book.status);
+			$(id).text(data.book.status);
+	    }
+	});
 });
+
